0ff9adca485c7cadddc0cb5a5f85e4b8679c892c
Yourii2 <yourii2@github.com>
2026-09-23 04:43:53 +0300

    Exclude batch files from release package to prevent file lock during auto-update

--- Release.bat
+++ Release.bat
@@ -98,12 +98,9 @@ if exist "Dragon.png" copy /y "Dragon.png" "%STAGE%\Dragon.png" >nul
 if exist "metadata.json" copy /y "metadata.json" "%STAGE%\metadata.json" >nul
 if exist "update-config.json" copy /y "update-config.json" "%STAGE%\update-config.json" >nul
 if exist "version.json" copy /y "version.json" "%STAGE%\version.json" >nul
-if exist "install.bat" copy /y "install.bat" "%STAGE%\install.bat" >nul
-if exist "start.bat" copy /y "start.bat" "%STAGE%\start.bat" >nul
-if exist "restart.bat" copy /y "restart.bat" "%STAGE%\restart.bat" >nul
-if exist "stop.bat" copy /y "stop.bat" "%STAGE%\stop.bat" >nul
-if exist "recreate_license.bat" copy /y "recreate_license.bat" "%STAGE%\recreate_license.bat" >nul
-if exist "update_and_restart.bat" copy /y "update_and_restart.bat" "%STAGE%\update_and_restart.bat" >nul
+
+rem DO NOT ship batch files (.bat) - start.bat is locked while running during updates
+for /r "%STAGE%" %%b in (*.bat) do del /f /q "%%b" >nul 2>nul
 
 rem DO NOT ship customer-specific files
 if exist "%STAGE%\config.php" del /f /q "%STAGE%\config.php" >nul 2>nul
